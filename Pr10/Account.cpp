8a79207cb1483b600b323c49cf75d1c53656c05b
MightyChipmunk <35093963+MightyChipmunk@users.noreply.github.com>
2020-04-18 18:56:57 +0900

    프로젝트 10단계

---
+++ Pr10/Account.cpp
@@ -0,0 +1,34 @@
+#include "BankingCommonDecl.h"
+#include "Account.h"
+
+Account::Account(int num, String name, int money) : Accnum(num), Accmoney(money), Accname(name) {}
+
+Account::Account() {
+	Accnum = 0;
+	Accname;
+	Accmoney = 0;
+} //객체 배열 선언을 위한 생성자, 필요는 없어졌지만 그냥 나둠
+
+int Account::GetNum() const {
+	return Accnum;
+} //const로 선언이 가능한 함수
+
+int Account::GetMoney() const {
+	return Accmoney;
+}
+
+void Account::AddMoney(int num) {
+	Accmoney += num;
+}
+
+void Account::SubMoney(int num) {
+	Accmoney -= num;
+}
+
+void Account::PrintAccount() const {
+	cout << "계좌ID: " << Accnum << endl;
+	cout << "이름: " << Accname << endl;
+	cout << "잔액: " << Accmoney << endl << endl;
+} //const로 선언이 가능한 함수
+
+
---
+++ Pr10/AccountHandler.cpp
@@ -0,0 +1,112 @@
+#include "AccountHandler.h"
+#include "NormalAccount.h"
+#include "HighCreditAccount.h"
+#include "BankingCommonDecl.h"
+
+AccountHandler::AccountHandler() {
+	index = 0;
+}
+
+void AccountHandler::ShowMenu() const {
+	cout << "-----Menu-----" << endl;
+	cout << "1. 계좌개설" << endl;
+	cout << "2. 입금" << endl;
+	cout << "3. 출금" << endl;
+	cout << "4. 계좌정보 전체 출력" << endl;
+	cout << "5. 프로그램 종료" << endl;
+	cout << "선택 ";
+}
+
+void AccountHandler::MakeAccount() {
+	int choice;
+	cout << "1.보통예금계좌 2.신용신뢰계좌" << endl;
+	cout << "선택: "; cin >> choice;
+	if (choice == 1) {
+		MakeNormalAccount();
+	}
+	else if (choice == 2) {
+		MakeHighCreditAccount();
+	}
+	else
+		cout << "잘못된 선택입니다." << endl;
+}
+
+void AccountHandler::MakeNormalAccount() {
+	int num, money, interest;
+	String name;
+	cout << "[보통예금계좌 개설]" << endl;
+	cout << "계좌ID: "; cin >> num;
+	for (int i = 0; i < index; i++) {
+		if (acc[i]->GetNum() == num) {
+			cout << "이미 존재하는 ID입니다." << endl;
+			return;
+		}
+	}
+	cout << "이 름: "; cin >> name;
+	cout << "입금액: "; cin >> money;
+	cout << "이자율: "; cin >> interest;
+	acc[index] = new NormalAccount(num, name, money, interest);
+	cout << endl;
+	index++;
+}
+
+void AccountHandler::MakeHighCreditAccount() {
+	int num, money, interest, grade;
+	String name;
+	cout << "[신뢰예금계좌 개설]" << endl;
+	cout << "계좌ID: "; cin >> num;
+	for (int i = 0; i < index; i++) {
+		if (acc[i]->GetNum() == num) {
+			cout << "이미 존재하는 ID입니다." << endl;
+			return;
+		}
+	}
+	cout << "이 름: "; cin >> name;
+	cout << "입금액: "; cin >> money;
+	cout << "이자율: "; cin >> interest;
+	cout << "신용등급(1toA, 2toB, 3toC): "; cin >> grade;
+	acc[index] = new HighCreditAccount(num, name, money, interest, grade);
+	cout << endl;
+	index++;
+}
+
+void AccountHandler::DepositMenu() {
+	int num, money;
+	cout << "[입금]" << endl;
+	cout << "계좌ID: "; cin >> num;
+	for (int i = 0; i < index; i++) {
+		if (acc[i]->GetNum() == num) {
+			cout << "입금액: "; cin >> money;
+			acc[i]->AddMoney(money);
+			cout << "입금완료" << endl << endl;
+			return;
+		}
+	}
+	cout << "잘못된 입력입니다." << endl << endl;
+}
+
+void AccountHandler::WithdrawMenu() {
+	int num, money;
+	cout << "[출금]" << endl;
+	cout << "계좌ID: "; cin >> num;
+	for (int i = 0; i < index; i++) {
+		if (acc[i]->GetNum() == num) {
+			cout << "출금액: "; cin >> money;
+			acc[i]->SubMoney(money);
+			cout << "출금완료" << endl << endl;
+			return;
+		}
+	}
+	cout << "잘못된 입력입니다." << endl << endl;
+}
+
+void AccountHandler::Print() const {
+	for (int i = 0; i < index; i++) {
+		acc[i]->PrintAccount();
+	}
+}
+
+AccountHandler::~AccountHandler() {
+	for (int i = 0; i < index; i++)
+		delete acc[i];
+}
